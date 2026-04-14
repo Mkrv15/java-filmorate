9f2b86879465de01624802bace84df6baa39053f
Ilya Makarov <Ilya15040@yandex.ru>
2026-04-14 23:24:12 +0400

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.user.UserService;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-
 import java.util.List;
 
 @RestController
